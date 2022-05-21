5c97c7265a1a1fef8e03489467b824874b3bdf36
panda <moad.tahri@gmail.com>
2022-05-21 14:21:08 -0700

    project birth

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project objective is to explore the problem of customer lifetime value in a contractual setting',
+    author='Tahri',
+    license='',
+)
